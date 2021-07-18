73ec0a426d7fa0acc9ef4bed5f978faad14ce1db
Lucas Winther <lucasw89@live.dk>
2021-07-18 18:14:07 +0200

    Removed unnecessary endpoint

--- main.go
+++ main.go
@@ -3,24 +3,20 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
-	"net/http"
 )
 
 func main() {
 	r := gin.Default()
 	m := melody.New()
 
-	r.GET("/", func(c *gin.Context) {
-		http.ServeFile(c.Writer, c.Request, "index.html")
-	})
-
 	r.GET("/:name/ws", func(c *gin.Context) {
 		m.HandleRequest(c.Writer, c.Request)
 	})
 
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		// Send message to all participants on url other than me.
 		m.BroadcastFilter(msg, func(q *melody.Session) bool {
-			return q.Request.URL.Path == s.Request.URL.Path
+			return (q.Request.URL.Path == s.Request.URL.Path) && (s != q)
 		})
 	})
 
